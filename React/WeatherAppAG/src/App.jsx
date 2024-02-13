090d470c779c8a91568f0d16a365c610c1855771
apoorwagupta <apoorwa.gupta01@gmail.com>
2024-02-14 03:21:06 +0530

    complete weather app

--- React/WeatherAppAG/src/App.jsx
+++ React/WeatherAppAG/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   // weatherData is the fetched data
 
   useEffect(() => {
-    const ApiURL = 'https://api.weatherapi.com/v1/current.json?key=cee3700c835849e0b1d223248241202&q=${city}&aqi=no';
+    const ApiURL = `https://api.weatherapi.com/v1/current.json?key=cee3700c835849e0b1d223248241202&q=${city}&aqi=no;`;
     fetch(ApiURL)
       // response is int the form of stream of data
       .then((response) => {
@@ -33,30 +33,59 @@ function App() {
   // align - items: shorter word: vertical alignment
   return (
     <div className='bg-[#1F213A] h-screen flex justify-center align-top'>
+    
       {/* // 1/5 width of the available width */}
 
       <div className='mt-40 w-1/5 h-1/3' >
-        <Temperature>
-          setCity = {setCity}
-          stats ={
-            {
-              temp: weatherData.current.temp_c,
-              condition: weatherData.current.condition.text,
-              isDay: weatherData.current.is_day,
-              location: weatherData.location.name,
-              time: weatherData.location.localtime,
-            }
-          }
-        </Temperature>
+        {/* only when weatherdata fetched */}
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current?.temp_c,
+              condition: weatherData.current?.condition.text,
+              isDay: weatherData.current?.is_day,
+              location: weatherData.location?.name,
+              time: weatherData.location?.localtime,
+            }}
+          />
+        )}
           
       </div> 
       <div className='mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6'>
         <h2 className='text-slate-200 text-2xl col-span-2'>Today's Highlights</h2>
         {/* gives our heading the span of two coloumns complete */}
-        <Highlights />
-        <Highlights />
-        <Highlights />
-        <Highlights />
+        {weatherData &&
+        (
+          <>
+            <Highlights stats={{
+              title: "Wind Status",
+              value: weatherData.current?.wind_mph,
+              unit: "mph",
+              direction: weatherData.current?.wind_dir, 
+            }}
+            />
+            <Highlights stats={{
+              title: "Humidity",
+              value: weatherData.current?.humidity,
+              unit: "%",
+            }}
+            />
+            <Highlights stats={{
+              title: "Visibility",
+              value: weatherData.current?.vis_miles,
+              unit: "miles",
+            }} />
+            <Highlights stats={{
+              title: "Air Pressure",
+              value: weatherData.current?.pressure_mb,
+              unit: "mb",
+            }} />
+          </>
+        )
+          
+        }
+        
       </div>
     </div>
     
